35b72b5dcd01e77cd4f193638d869fe9bded9fb3
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2021-10-06 16:56:08 +0700

    Initial commit

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float xInitialForce;
+    public float yInitialForce;
+
+    private Vector2 trajectoryOrigin;
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        trajectoryOrigin = transform.position;
+        RestartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResetBall()
+    {
+        transform.position = Vector2.zero;
+
+        rb.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        float randomDirection = Random.Range(0, 2);
+
+        if (randomDirection < 1.0f)
+        {
+            rb.AddForce(new Vector2(-xInitialForce, 1));
+        }
+        else
+        {
+            rb.AddForce(new Vector2(xInitialForce, 1));
+        }
+    }
+
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("PushBall", 2);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+}
